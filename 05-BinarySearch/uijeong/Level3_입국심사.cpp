895e0f5cc309bcba5d4f97f6719833e70d7aab81
Justice <juijeong8324@gmail.com>
2023-11-07 20:48:05 +0900

    Update Level3_입국심사.cpp

--- 05-BinarySearch/uijeong/Level3_입국심사.cpp
+++ 05-BinarySearch/uijeong/Level3_입국심사.cpp
@@ -1,54 +1,36 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int N;
-int b;
-int budget[100001];
-
-bool checkSum(int s) {
-    int sum = 0;
-
-    for (int i = 0; i < N; i++) {
-        if (budget[i] >= s) {
-            // 예산 이상인 경우 
-            sum += s;
-        }
-        else {
-            sum += budget[i];
-        }
+bool checkT(int mid, int n, vector<int> times){
+    iong long count = 0;
+    for(int i = 0; i < times.size(); i++){
+        count += mid / times[i]; 
     }
-
-    return sum <= b; // 합계가 맞을 때 true
+    
+    return count >= n;
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> N;
-    int sum = 0;
-    int m = 0;
-    for (int i = 0; i < N; i++) {
-        cin >> budget[i];
-        sum += budget[i];
-        m = max(m, budget[i]);
-    }
-    cin >> b;
-
-    if (sum <= b) { // 주어진 예산 안이면 
-        cout << m;
-    }
-    else {
-        int st = 1;
-        int ed = b;
-
-        while (st < ed) {
-            int mid = (st + ed + 1) / 2; // st와 ed가 1 차이나면 st가 계속 똑같아버림
-
-            if (checkSum(mid)) st = mid; // 예산을 좀더 늘려도 됨
-            else ed = mid - 1; // mid보다 작은 예산에서 찾아야 하므로 
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    sort(times.begin(), times.end()); // 오름차순 
+    
+    long long st = 1;
+    long long end = times[times.size()-1] * n; // 최대 시간 
+    
+    while(st <= end){
+        long long mid = (st+end)/2;
+        
+        if(checkT(mid, n, times)){
+            // 사람 명수가 더 많으면 
+            answer = mid;
+            end = mid-1; // mid를 줄여봐야 
         }
-
-        cout << st;
+        else{
+            // 적으면
+            st = mid+1; // mid를 더 늘려야..
+        }
     }
+    
+    return answer;
 }
