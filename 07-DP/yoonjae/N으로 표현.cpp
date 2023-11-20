73c6e4bb4e5064dfb03b4c6e8ca05a9400b1ecc9
yoon-H <ucando0320@gmail.com>
2023-11-20 18:34:06 +0900

    [ADD] N으로 표현
    [ADD] N으로 표현

---
+++ 07-DP/yoonjae/N으로 표현.cpp
@@ -0,0 +1,58 @@
+﻿#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(int N, int number) {
+    if (N == number) return 1;
+
+    vector<set<int>> v;
+    set<int> first;
+
+    first.insert(N);
+    v.push_back(first);
+
+    for (int i = 1; i < 8; i++)
+    {
+        set<int> vec;
+
+        int num = N;
+        for (int j = 0; j < i; j++)
+        {
+            num = num * 10 + N;
+        }
+
+        vec.insert(num);
+
+        for (int j = 0; j < i; j++)
+        {
+            for (int k = 0; k < i; k++)
+            {
+                if (j + k + 1 != i) continue;
+                else
+                {
+                    for (int num1 : v[j])
+                    {
+                        for (int num2 : v[k])
+                        {
+                            vec.insert(num1 + num2);
+
+                            if (num1 - num2 >= 0) vec.insert(num1 - num2);
+
+                            vec.insert(num1 * num2);
+
+                            if (num2 != 0) vec.insert(num1 / num2);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (vec.find(number) != vec.end()) return i + 1;
+
+        v.push_back(vec);
+    }
+
+    return -1;
+}
