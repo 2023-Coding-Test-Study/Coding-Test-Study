f0faea5e5647d5d4aa535f7c196bfb502d418c42
leeeha <jxlhe46@gmail.com>
2023-12-25 22:32:09 +0900

    [FIX] 감시 문제 오답 코드 수정

--- 11-Implementation/haeun/15683_감시.cpp
+++ 11-Implementation/haeun/15683_감시.cpp
@@ -6,129 +6,184 @@
 using namespace std;
 typedef pair<int, int> pii;
 
-const int MAX = 8;
+const int MAX = 9;
 int N, M;
 
-int graph[MAX][MAX];
-vector<pii> cctv;
+int graph[MAX][MAX]; 
+int temp[MAX][MAX]; 
+vector<pair<pii, pii>> cctv;
 
-int ans = MAX * MAX;
-int cctvCount = 0; 
+int ans = 1e9;
+int cctvCount = 0;
 
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
+// 동서남북 
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 
 void input() {
 	cin >> N >> M;
 
-	// 0: 빈칸, 6: 벽, 1~5: CCTV 
+	// 0: 빈칸, 6: 벽, 1~5: CCTV
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < M; j++){
 			cin >> graph[i][j];
-
-			// cctv에 관한 정보 저장 
+			
 			if(graph[i][j] >= 1 && graph[i][j] <= 5){
-				cctv.push_back({i, j});
+				// {{x, y}, {번호, 방향}}
+				cctv.push_back({{i, j}, {graph[i][j], -1}});
 			}
 		}
 	}
 
+	// cctv 총 개수 초기화 
 	cctvCount = cctv.size();
 }
 
-void checkVisibleArea(int dir, int x, int y) {
-	dir %= 4; // 0~3 중에 방향 설정하기
-
-	while(true){
-		// dir 방향으로 직진하다가 
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
-
-		// 벽을 마주치면 종료 
-		if(nx < 0 || nx >= N || ny < 0 || ny >= M || 
-			graph[nx][ny] == 6) break;
-
-		// cctv로 감시 가능한 영역 표시하기 
-		if(graph[nx][ny] == 0){
-			graph[nx][ny] = -1;
-		}
-
-		// 다음 위치 갱신 
-		x = nx; 
-		y = ny; 
-	}
-}
-
-void updateMinUnvisibleAreaSize() {
-	int cnt = 0;
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-			if(graph[i][j] == 0) cnt++; 
-		}
-	}
-	ans = min(ans, cnt);
-}
-
-void setDirection(int idx) {
-	if(idx == cctvCount){
-		updateMinUnvisibleAreaSize(); 
-		return;
-	}
-
-	int x = cctv[idx].first;
-	int y = cctv[idx].second;
-
-	// 복사본 배열 초기화 
-	int temp[N][M];
+void copyGraphArray() {
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < M; j++){
 			temp[i][j] = graph[i][j]; 
 		}
 	}
+}
 
-	for(int dir = 0; dir < 4; dir++){
-		// 변경된 그래프 상태 반영 
-		for(int i = 0; i < N; i++){
-			for(int j = 0; j < M; j++){
-				temp[i][j] = graph[i][j]; 
+void moveRight(int x, int y){
+	for(int i = y + 1; i < M; i++){
+		if(temp[x][i] == 6) break; 
+		if(temp[x][i] >= 1 && temp[x][i] <= 5) continue; 
+		temp[x][i] = -1; 
+	}
+}
+
+void moveLeft(int x, int y){
+	for(int i = y - 1; i >= 0; i--){
+		if(temp[x][i] == 6) break; 
+		if(temp[x][i] >= 1 && temp[x][i] <= 5) continue; 
+		temp[x][i] = -1; 
+	}
+}
+
+void moveBottom(int x, int y){
+	for(int i = x + 1; i < N; i++){
+		if(temp[i][y] == 6) break; 
+		if(temp[i][y] >= 1 && temp[i][y] <= 5) continue; 
+		temp[i][y] = -1; 
+	}
+}
+
+void moveTop(int x, int y){
+	for(int i = x - 1; i >= 0; i--){
+		if(temp[i][y] == 6) break; 
+		if(temp[i][y] >= 1 && temp[i][y] <= 5) continue; 
+		temp[i][y] = -1; 
+	}
+}
+
+void checkVisibleArea() {
+	copyGraphArray(); 
+
+	for(int i = 0; i < cctv.size(); i++){
+		int x = cctv[i].first.first;
+		int y = cctv[i].first.second; 
+		int num = cctv[i].second.first;
+		int dir = cctv[i].second.second; 
+
+		// 동서남북 중에 한 방향 
+		if(num == 1){
+			if(dir == 0){
+				moveRight(x, y);
+			}else if(dir == 1){
+				moveLeft(x, y);
+			}else if(dir == 2){
+				moveBottom(x, y);
+			}else if(dir == 3){
+				moveTop(x, y);
 			}
 		}
-		
-		// cctv 종류에 따라 감시 가능한 영역 표시 
-		if(graph[x][y] == 1){ // 한 방향
-			checkVisibleArea(dir, x, y);
-		}
-		else if(graph[x][y] == 2){ // 180도 방향 
-			checkVisibleArea(dir, x, y);
-			checkVisibleArea(dir + 2, x, y);
-		}
-		else if(graph[x][y] == 3){ // 90도 방향 
-			checkVisibleArea(dir, x, y);
-			checkVisibleArea(dir + 1, x, y);
-		}
-		else if(graph[x][y] == 4){ // 세 방향 
-			checkVisibleArea(dir, x, y);
-			checkVisibleArea(dir + 1, x, y);
-			checkVisibleArea(dir + 2, x, y);
-		}
-		else if(graph[x][y] == 5){ // 네 방향 
-			checkVisibleArea(dir, x, y);
-			checkVisibleArea(dir + 1, x, y);
-			checkVisibleArea(dir + 2, x, y);
-			checkVisibleArea(dir + 3, x, y);
-		}
-
-		// 그 다음 cctv에 대한 방향 설정
-		setDirection(idx + 1);
-
-		// 모든 cctv에 대한 방향 설정이 끝나면 
-		// 그 다음 경우의 수를 위해 원본 배열 복원하기 
-		for(int i = 0; i < N; i++){
-			for(int j = 0; j < M; j++){
-				graph[i][j] = temp[i][j]; 
+		// 동서 또는 남북 중에 한 방향 
+		else if(num == 2){
+			if(dir == 0 || dir == 1){
+				moveRight(x, y);
+				moveLeft(x, y);
+			}else if(dir == 2 || dir == 3){
+				moveBottom(x, y);
+				moveTop(x, y);
 			}
+		}
+		// 90도 방향 
+		else if(num == 3){
+			if(dir == 0){
+				moveRight(x, y);
+				moveTop(x, y);
+			}else if(dir == 1){
+				moveLeft(x, y);
+				moveBottom(x, y);
+			}else if(dir == 2){
+				moveRight(x, y);
+				moveBottom(x, y);
+			}else if(dir == 3){
+				moveLeft(x, y);
+				moveTop(x, y);
+			}
+		}
+		// 세 방향
+		else if(num == 4){
+			if(dir == 0){
+				moveRight(x, y);
+				moveTop(x, y);
+				moveBottom(x, y);
+			}else if(dir == 1){
+				moveLeft(x, y);
+				moveTop(x, y);
+				moveBottom(x, y);
+			}else if(dir == 2){
+				moveRight(x, y);
+				moveLeft(x, y);
+				moveBottom(x, y);
+			}else if(dir == 3){
+				moveRight(x, y);
+				moveLeft(x, y);
+				moveTop(x, y);
+			}
+		}
+		// 동서남북 모든 방향 
+		else if(num == 5){
+			moveRight(x, y);
+			moveLeft(x, y);
+			moveBottom(x, y);
+			moveTop(x, y);
 		}
 	}
+}
+
+int getUnvisibleAreaSize() {
+	int cnt = 0;
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < M; j++){
+			if(temp[i][j] == 0) cnt++; 
+		}
+	}
+	return cnt;
+}
+
+void setDirection(int idx) {
+	if(idx == cctvCount){
+		checkVisibleArea(); 
+		ans = min(ans, getUnvisibleAreaSize());
+		return;
+	}
+
+	cctv[idx].second.second = 0; 
+	setDirection(idx + 1);
+
+	cctv[idx].second.second = 1; 
+	setDirection(idx + 1);
+
+	cctv[idx].second.second = 2; 
+	setDirection(idx + 1);
+
+	cctv[idx].second.second = 3; 
+	setDirection(idx + 1);
 }
 
 int main()
