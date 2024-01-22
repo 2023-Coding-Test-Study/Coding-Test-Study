6d08641db3bffb974a54ff0a837b802cdefa2c0c
leeeha <jxlhe46@gmail.com>
2024-01-22 16:50:57 +0900

    [ADD] 여행 경로 문제 추가

---
+++ week14/haeun/여행경로.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+typedef pair<string, int> psi; // 공항 이름, 노드 번호  
+
+const int MAX = 10001;
+unordered_map<string, vector<psi>> graph; 
+bool visited[MAX];
+
+vector<string> answer;
+int totalNum = 0;
+
+// 가능한 경우의 수 중에 첫번째 것만 저장하기 위한 플래그 변수 
+bool foundFirstCase = false; 
+
+void dfs(string now, int cnt, vector<string> cache) {
+    if(cnt == totalNum && !foundFirstCase) {
+        foundFirstCase = true;
+        answer = cache;
+        return;
+    }
+    
+    for(auto edge: graph[now]){
+        string name = edge.first; 
+        int idx = edge.second;
+        
+        if(!visited[idx]){
+            // 방문 처리 
+            visited[idx] = true; 
+            cache.push_back(name);
+            
+            // 재귀 호출 
+            dfs(name, cnt + 1, cache);
+            
+            // 상태 복구
+            cache.pop_back();
+            visited[idx] = false;
+        }
+    }
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+    // 알파벳 순으로 오름차순 정렬
+    sort(tickets.begin(), tickets.end());
+    totalNum = tickets.size(); 
+    
+    // 노드 간의 연결 관계 -> 해시에 저장
+    for(int i = 0; i < tickets.size(); i++){
+        string s = tickets[i][0];
+        string e = tickets[i][1];
+        graph[s].push_back({e, i});
+    }
+    
+    dfs("ICN", 0, {"ICN"});
+    
+    return answer;
+}
