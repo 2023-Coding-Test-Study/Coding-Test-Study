21d1d65560721d813418f5161cf404aa30eed3c4
leeeha <jxlhe46@gmail.com>
2024-01-24 00:18:28 +0900

    [MOD] 여행경로 문제 주석 수정

--- week14/haeun/여행경로.cpp
+++ week14/haeun/여행경로.cpp
@@ -4,20 +4,20 @@
 #include <algorithm>
 #include <unordered_map>
 using namespace std;
-typedef pair<string, int> psi; // 공항 이름, 노드 번호  
+typedef pair<string, int> psi; // 공항 이름, 간선 번호
 
 const int MAX = 10001;
-unordered_map<string, vector<psi>> graph; 
+unordered_map<string, vector<psi>> graph;
 bool visited[MAX];
 
 vector<string> answer;
-int totalNum = 0;
+int maxLength = 0; // 그래프 가지의 최대 깊이 (간선의 최대 개수)
 
 // 가능한 경우의 수 중에 첫번째 것만 저장하기 위한 플래그 변수 
 bool foundFirstCase = false; 
 
 void dfs(string now, int cnt, vector<string> cache) {
-    if(cnt == totalNum && !foundFirstCase) {
+    if(cnt == maxLength && !foundFirstCase) {
         foundFirstCase = true;
         answer = cache;
         return;
@@ -28,11 +28,11 @@ void dfs(string now, int cnt, vector<string> cache) {
         int idx = edge.second;
         
         if(!visited[idx]){
-            // 방문 처리 
+            // 방문 처리
             visited[idx] = true; 
             cache.push_back(name);
             
-            // 재귀 호출 
+            // 재귀 호출
             dfs(name, cnt + 1, cache);
             
             // 상태 복구
@@ -45,7 +45,7 @@ void dfs(string now, int cnt, vector<string> cache) {
 vector<string> solution(vector<vector<string>> tickets) {
     // 알파벳 순으로 오름차순 정렬
     sort(tickets.begin(), tickets.end());
-    totalNum = tickets.size(); 
+    maxLength = tickets.size();
     
     // 노드 간의 연결 관계 -> 해시에 저장
     for(int i = 0; i < tickets.size(); i++){
